2c76fc5802046361f3dc0f40efc6720d8617b610
Simão Gomes <1191061@isep.ipp.pt>
2024-05-05 13:00:11 +0100

    test: test password encryptor

--- Backend/StreamingService.Test/Dao/GenericRepositoryTest.cs
+++ Backend/StreamingService.Test/Dao/GenericRepositoryTest.cs
@@ -3,6 +3,7 @@ using StreamingPlatform.Dao;
 using StreamingPlatform.Dao.Repositories;
 using StreamingPlatform.Models;
 using StreamingPlatform.Models.Enums;
+using StreamingPlatform.Utils;
 namespace StreamingService.Test.DaoTesting
 {
     [TestClass]
@@ -46,6 +47,34 @@ namespace StreamingService.Test.DaoTesting
             Assert.IsTrue(activePlans.Count == 3);
             Assert.IsTrue(firstActivePlan.Count == 1);
             Assert.IsTrue(inactivePlan.Count == 1);
+        }
+
+        [TestMethod]
+        public void CreateTest()
+        {
+
+            if (this.context == null)
+            {
+                Assert.Fail("Context is null");
+            }
+            string userPassword = Faker.Lorem.Sentence().Trim();
+
+
+            UnitOfWork unitOfWork = new(this.context);
+            GenericRepository<User> userRepository = (GenericRepository<User>)unitOfWork.Repository<User>();
+            string pepper = PasswordEncryptor.GenerateSalt(); // TODO: Use the value from the configuration file/secrets instead of generating it.
+            User user = new()
+            {
+                UserName = "test",
+                Email = Faker.Internet.Email(),
+                Password = new Password(userPassword, pepper)
+            };
+            userRepository.Create(user);
+            unitOfWork.SaveChanges();
+            List<User> users = userRepository.GetAllRecords().ToList();
+            Assert.IsTrue(users.Count == 1);
+            Assert.IsTrue(users[0].UserName == user.UserName);
+            Assert.IsTrue(users[0].Email == user.Email);
         }
 
         private void SetupMockData(GenericRepository<Plan> repository, UnitOfWork unitOfWork)
---
+++ Backend/StreamingService.Test/Utils/PasswordEncryptorTest.cs
@@ -0,0 +1,48 @@
+﻿using StreamingPlatform.Utils;
+
+namespace StreamingService.Test.Utils
+{
+    [TestClass]
+    public class PasswordEncryptorTests
+    {
+        private const string TestPassword = "P@ssw0rd";
+        private const string TestPepper = "SecretPepper";
+
+        [TestMethod]
+        public void EncryptPasswordValidInputReturnsNonEmptyString()
+        {
+            string salt = PasswordEncryptor.GenerateSalt();
+            int iteration = 100;
+            string encryptedPassword = PasswordEncryptor.EncryptPassword(TestPassword, salt, TestPepper, iteration);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(encryptedPassword));
+        }
+
+        [TestMethod]
+        public void GenerateSaltReturnsValidSalt()
+        {
+            string salt = PasswordEncryptor.GenerateSalt();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(salt));
+            Assert.IsTrue(salt.Length > 0);
+        }
+
+        [TestMethod]
+        public void EncryptPasswordWithZeroIterationsReturnsOriginalPassword()
+        {
+            string salt = PasswordEncryptor.GenerateSalt();
+            int iteration = 0;
+            string encryptedPassword = PasswordEncryptor.EncryptPassword(TestPassword, salt, TestPepper, iteration);
+            Assert.AreEqual(TestPassword, encryptedPassword);
+        }
+
+        [TestMethod]
+        public void EncryptPasswordSameInputAndParametersReturnsSameOutput()
+        {
+            string salt = PasswordEncryptor.GenerateSalt();
+            int iteration = 50;
+            string encryptedPassword1 = PasswordEncryptor.EncryptPassword(TestPassword, salt, TestPepper, iteration);
+            string encryptedPassword2 = PasswordEncryptor.EncryptPassword(TestPassword, salt, TestPepper, iteration);
+            Assert.AreEqual(encryptedPassword1, encryptedPassword2);
+        }
+    }
+
+}
